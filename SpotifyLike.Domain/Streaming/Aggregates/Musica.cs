6d2ebd783c5c95d4d0cff8c5dedf9a875e084751
Caio Albuquerque <caio.albuquerque@smarthis.com>
2023-12-09 11:17:09 -0300

    Add project files.

---
+++ SpotifyLike.Domain/Streaming/Aggregates/Musica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotifyLike.Domain.Streaming.ValueObject;
+
+namespace SpotifyLike.Domain.Streaming.Aggregates
+{
+    public class Musica
+    {
+        public Guid Id { get; set; }
+        public String Nome { get; set; }
+        public Duracao Duracao { get; set; }
+
+    }
+}
