ed72074a1b3775e8c8a984fcce2605404c973334
Caio Albuquerque <caio.albuquerque@smarthis.com>
2023-12-16 11:15:20 -0300

    Criação de usuário adicionada

--- SpotifyLike.Domain/Conta/Aggregates/Usuario.cs
+++ SpotifyLike.Domain/Conta/Aggregates/Usuario.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpotifyLike.Domain.Streaming.Aggregates;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
 
 namespace SpotifyLike.Domain.Conta.Aggregates
 {
     public class Usuario
     {
+
+        private const string NOME_PLAYLIST = "Favoritas";
+
         public Guid Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
@@ -19,5 +25,79 @@ namespace SpotifyLike.Domain.Conta.Aggregates
 
         public List<Playlist> Playlists { get; set; } = new List<Playlist>();
 
+        public void CriarConta(string nome, string email, string senha, DateTime dtNascimento, Plano plano, Cartao cartao)
+        {
+            this.Nome = nome;
+            this.Email = email;
+
+            //Todo: Criptografar a senha
+            this.Senha = this.CriptografarSenha(senha);
+
+            this.DtNascimento = dtNascimento;
+
+            //Assinar um plano
+            this.AssinarPlano(plano, cartao);
+
+            //Adicionar cartão na conta do usuário
+            this.AdicionarCartao(cartao);
+
+            //Criar a playlist padrão do usuário
+            this.CriarPlaylist(nome: NOME_PLAYLIST, publica: false);
+        }
+
+        public void CriarPlaylist(string nome, bool publica = true)
+        {
+            this.Playlists.Add(new Playlist()
+            {
+                Nome = nome,
+                Publica = false,
+                DtCriacao = DateTime.Now,
+                Usuario = this
+            });
+        }
+
+        private void AdicionarCartao(Cartao cartao)
+            => this.Cartoes.Add(cartao);
+
+        private void AssinarPlano(Plano plano, Cartao cartao)
+        {
+            //Debitar o valor do plano no cartao
+
+            cartao.CriarTransacao(new Transacao.ValueObject.Merchant() { Nome = plano.Nome }, new Core.ValueObject.Monetario(plano.Valor), plano.Descricao);
+
+            //Desativo caso tenha alguma assinatura ativa
+            DesativarAssinaturaAtiva();
+
+            //Adiciona nova assinatura
+            this.Assinaturas.Add(new Assinatura()
+            {
+                Ativo = true,
+                Plano = plano,
+                DtAtivacao = DateTime.Now,
+
+            });
+        }
+
+        private void DesativarAssinaturaAtiva()
+        {
+            if (this.Assinaturas.Count > 0 && this.Assinaturas.Any(x => x.Ativo))
+            {
+                var planoAtivo = this.Assinaturas.FirstOrDefault(x => x.Ativo);
+                planoAtivo.Ativo = false;
+            }
+        }
+
+        private String CriptografarSenha(string senhaAberta)
+        {
+            //Bcript seria a melhor biblioteca pra armazenar senha
+
+            SHA256 criptoProvider = SHA256.Create();
+
+            byte[] btexto = Encoding.UTF8.GetBytes(senhaAberta);
+
+            var criptoResult = criptoProvider.ComputeHash(btexto);
+
+            return Convert.ToHexString(criptoResult);
+        }
     }
 }
