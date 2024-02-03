4e26789b20236e9d7beb2473f355e95ed2b20a7f
caio.albuquerque <caio.albuquerque@al.edu.infnet.br>
2024-02-03 15:03:38 -0300

    Inicio parametrização de APIs

---
+++ SpotifyLike.API/Controllers/BandaController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpotifyLike.Domain.Streaming.Aggregates;
+using SpotifyLike.Repository;
+
+namespace SpotifyLike.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BandaController : ControllerBase
+    {
+
+        private SpotifyLikeContext Context{ get; set; }
+
+        public BandaController(SpotifyLikeContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetBandas() 
+        {
+            var result = this.Context.Bandas.ToList();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetBandas(Guid id) 
+        {
+
+            var result = this.Context.Bandas.Where(x => x.Id == id).FirstOrDefault();
+
+            if (result == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult SaveBanda([FromBody] Banda banda)
+        {
+            this.Context.Bandas.Add(banda);
+            this.Context.SaveChanges();
+
+            return Created($"/banda/{banda.Id}", banda);
+        }
+    }
+}
