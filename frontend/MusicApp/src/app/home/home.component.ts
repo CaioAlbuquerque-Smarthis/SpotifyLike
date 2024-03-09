0acbe5d2e64e8db58c52d42c6048d55e80654b40
caio.albuquerque <caio.albuquerque@al.infnet.edu.br>
2024-03-09 15:22:34 -0300

    Final da aula 09/03, front end funcional mostrando as bandas e com a rota para ver detalhes das bandas, ainda não mostrando os detalhes, só uma mensagem padrão

--- frontend/MusicApp/src/app/home/home.component.ts
+++ frontend/MusicApp/src/app/home/home.component.ts
@@ -5,11 +5,13 @@ import { BandaService } from '../services/banda.service';
 import { Banda } from '../model/banda';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, HttpClientModule, CommonModule],
+  imports: [MatButtonModule, MatCardModule, HttpClientModule, CommonModule, FlexLayoutModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,13 +19,17 @@ export class HomeComponent implements OnInit {
 
   bandas = null
 
-  constructor(private bandaService: BandaService) {
+  constructor(private bandaService: BandaService, private router:Router) {
   }
   ngOnInit(): void {
     this.bandaService.getBanda().subscribe(response => {
       console.log(response);
       this.bandas = response as any;
     });
+  }
+
+  public goToDetail(item:Banda){
+    this.router.navigate(["detail", item.id]);
   }
 
 }
