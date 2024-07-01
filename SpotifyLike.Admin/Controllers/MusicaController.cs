8d47ac8829b8ab0ff651f828f5a10f710424471e
Caio Albuquerque <caio.albuquerque@smarthis.com>
2024-06-30 22:30:28 -0300

    Tela de criar música implementada, precisando de ajuste no campo de seleção de banda e método para adicionar música

--- SpotifyLike.Admin/Controllers/MusicaController.cs
+++ SpotifyLike.Admin/Controllers/MusicaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Spotify.Application.Streaming;
+using SpotifyLike.Admin.Models;
 using SpotifyLike.Application.Streaming;
 
 namespace SpotifyLike.Admin.Controllers
@@ -7,14 +8,26 @@ namespace SpotifyLike.Admin.Controllers
     public class MusicaController : Controller
     {
         private MusicaService musicaService;
+        private BandaService bandaService;
 
-        public MusicaController(MusicaService musicaService) 
+        public MusicaController(MusicaService musicaService, BandaService bandaService) 
         {
             this.musicaService = musicaService;
+            this.bandaService = bandaService;
         }
         public IActionResult Index()
         {
             var result = this.musicaService.Obter();
+            return View(result);
+        }
+
+        public IActionResult Criar()
+        {
+            var bandas = this.bandaService.Obter();
+            var result = new MusicaViewModel
+            {
+                Bandas = bandas
+            };
             return View(result);
         }
     }
---
+++ SpotifyLike.Admin/Models/MusicaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SpotifyLike.Application.Streaming.Dto;
+
+namespace SpotifyLike.Admin.Models
+{
+    public class MusicaViewModel
+    {
+        public IEnumerable<BandaDto> Bandas { get; set; }
+        public String BandaId { get; set; }
+        public String NomeAlbum { get; set; }
+        public MusicaDto MusicaDto { get; set; }
+    }
+}
