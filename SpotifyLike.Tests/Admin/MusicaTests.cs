737dc2afaf03c8f0e696f88731fc14dec9a587f9
Caio Albuquerque <caio.albuquerque@smarthis.com>
2024-07-03 21:09:26 -0300

    Adição de um teste unitário da camada de admin, porém não foi validado

---
+++ SpotifyLike.Tests/Admin/MusicaTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SpotifyLike.Admin.Controllers;
+using SpotifyLike.Application.Streaming;
+using SpotifyLike.Application.Streaming.Dto;
+using SpotifyLike.Domain.Streaming.ValueObject;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifyLike.Tests.Admin
+{
+    public class MusicaTests
+    {
+        private readonly Mock<MusicaService> _musicaServiceMock;
+        private readonly Mock<AlbumService> _albumServiceMock;
+        private readonly MusicaController _controller;
+
+        public MusicaTests()
+        {
+            _musicaServiceMock = new Mock<MusicaService>();
+            _controller = new MusicaController(_musicaServiceMock.Object, _albumServiceMock.Object);
+        }
+
+        [Fact]
+        public void Index_ReturnsViewResult_WithAListOfMusicaDtos()
+        {
+            // Arrange
+            var musicaDtos = new List<MusicaDto>
+            {
+                new MusicaDto { Nome = "Song1", Duracao = new Duracao(200) },
+                new MusicaDto { Nome = "Song2", Duracao = new Duracao(300) }
+            };
+            _musicaServiceMock.Setup(service => service.Obter()).Returns(musicaDtos);
+
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<MusicaDto>>(viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+    }
+}
